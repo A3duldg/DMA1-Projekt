4132eefe759d82ef3fef02c041734b6de2f05976
jonatxan <164942565+A3duldg@users.noreply.github.com>
2025-04-07 14:13:55 +0200

    Add files via upload

---
+++ Design Project/Controller/LPCtrl.java
@@ -0,0 +1,35 @@
+package Controller;
+
+
+/**
+ * Lav en beskrivelse af klassen LPCtrl her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class LPCtrl
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    private int x;
+
+    /**
+     * Konstruktør for objekter af klassen LPCtrl
+     */
+    public LPCtrl()
+    {
+        // initialiser instansvariable
+        x = 0;
+    }
+
+    /**
+     * Et eksempel på en metode - erstat denne kommentar med din egen
+     * 
+     * @param  y  eksempel på en parameter til en metode
+     * @return    summen af x og y 
+     */
+    public int sampleMethod(int y)
+    {
+        // indsæt din egen kode her
+        return x + y;
+    }
+}
---
+++ Design Project/Controller/LoanCtrl.java
@@ -0,0 +1,35 @@
+package Controller;
+
+
+/**
+ * Lav en beskrivelse af klassen LoanCtrl her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class LoanCtrl
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    private int x;
+
+    /**
+     * Konstruktør for objekter af klassen LoanCtrl
+     */
+    public LoanCtrl()
+    {
+        // initialiser instansvariable
+        x = 0;
+    }
+
+    /**
+     * Et eksempel på en metode - erstat denne kommentar med din egen
+     * 
+     * @param  y  eksempel på en parameter til en metode
+     * @return    summen af x og y 
+     */
+    public int sampleMethod(int y)
+    {
+        // indsæt din egen kode her
+        return x + y;
+    }
+}
