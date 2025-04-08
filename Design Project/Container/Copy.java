6c40f7114e09480b7cf7218700a8abbd44a922c6
jonatxan <jondenlange@icloud.com>
2025-04-08 12:42:51 +0200

    copy

---
+++ Design Project/Container/Copy.java
@@ -0,0 +1,33 @@
+package Container;
+
+
+/**
+ * Lav en beskrivelse af klassen Copy her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class Copy
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    private int serialNumber;
+    private String purchaseDate;
+    private int purchasePrice;
+    
+
+    /**
+     * Konstruktør for objekter af klassen Copy
+     */
+    public Copy(int serialNumber, String purchaseDate, int purchasePrice)
+    {
+       this.serialNumber = serialNumber;
+       this.purchaseDate = purchaseDate;
+       this.purchasePrice = purchasePrice;
+       
+    }
+
+    public int getSerialNumber(){
+        return serialNumber;
+    }
+    }
+
