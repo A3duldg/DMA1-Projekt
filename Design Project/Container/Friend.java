396cca8fa87a600903ac6b5cdc988a4e22162549
Laurakjb <1086719@ucn.dk>
2025-04-08 13:01:07 +0200

    fg

--- Design Project/Container/Friend.java
+++ Design Project/Container/Friend.java
@@ -12,25 +12,39 @@ public class Friend
     // har sætter vi parrametrene til at finde vores friends
     private String name;
     private String phone;
+    private String address;
+    private int postalCode;
+    private String city;
+    
 
     // * Konstruktør for objekter af klassen Friend
     
-    public Friend(String name, String phone)
+    public Friend(String name, String phone, String address, int postalCode, String city)
     {
         // initialiser instansvariable
         this.name = name;
         this.phone = phone;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.city = city;
     }
 
     
     public String getName(){
         return name;
     }
-    
     public String getPhone(){
         return phone;
     }
-    
+    public String getAddress(){
+        return address;
+    }
+    public int getPostalCode(){
+        return postalCode;
+    }
+    public String getCity(){
+        return city;
+    }
     public void updateFriend (String name, String phone){
         this.name = name;
         this.phone = phone;
