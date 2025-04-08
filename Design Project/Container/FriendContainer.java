41100a0b6a0f57eb8ceb6a9a46022d07e8909c4e
Laurakjb <1086719@ucn.dk>
2025-04-08 11:06:19 +0200

    Update FriendContainer.java
    test

--- Design Project/Container/FriendContainer.java
+++ Design Project/Container/FriendContainer.java
@@ -11,6 +11,7 @@ public class FriendContainer
 {
     // instansvariabler - erstat eksemplet herunder med dine egne variabler
     private int x;
+    private name;
 
     /**
      * Konstruktør for objekter af klassen FriendContainer
